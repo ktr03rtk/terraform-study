8192c13425fff5ea2c4e74772133ba55efb10323
ktr03 <ktrsouth@gmail.com>
2021-07-01 01:44:36 +0900

    add case

--- 02/main.tf
+++ 02/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
+  ami                    = "ami-0c3fd0f5d33134a76"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
 
   tags = {
     "Name" = "example"
@@ -12,6 +13,26 @@ resource "aws_instance" "example" {
     systemctl start httpd.service
   EOF
 
+}
 
+output "example_public_dns" {
+  value = aws_instance.example.public_dns
+}
 
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }

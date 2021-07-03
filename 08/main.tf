3d994e87049568f5af8586f728545376d4583f91
ktr03 <ktrsouth@gmail.com>
2021-07-03 16:51:50 +0900

    done

--- 08/main.tf
+++ 08/main.tf
@@ -7,6 +7,10 @@ variable "elb_id" {
   default = "582318560864"
 }
 
+variable "domain_name" {
+  default = ""
+}
+
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
   port              = "80"
@@ -28,7 +32,7 @@ resource "aws_lb" "example" {
   load_balancer_type         = "application"
   internal                   = false
   idle_timeout               = 60
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   subnets = [
     aws_subnet.public_0.id,
@@ -74,6 +78,131 @@ module "http_redirect_sg" {
   port        = 8080
   cidr_blocks = ["0.0.0.0/0"]
 }
+
+data "aws_route53_zone" "example" {
+  name = var.domain_name
+}
+
+resource "aws_route53_zone" "test_example" {
+  name = "test.${var.domain_name}"
+}
+
+resource "aws_route53_record" "example" {
+  zone_id = data.aws_route53_zone.example.zone_id
+  name    = data.aws_route53_zone.example.name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.example.dns_name
+    zone_id                = aws_lb.example.zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "domain_name" {
+  value = aws_route53_record.example.name
+}
+
+resource "aws_acm_certificate" "example" {
+  domain_name               = aws_route53_record.example.name
+  subject_alternative_names = []
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+//resource "aws_route53_record" "example_certificate" {
+//  name    = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
+//  type    = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
+//  records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
+//  zone_id = data.aws_route53_zone.example.id
+//  ttl     = 60
+//}
+
+
+//resource "aws_acm_certificate_validation" "example" {
+//  certificate_arn         = aws_acm_certificate.example.arn
+//  validation_record_fqdns = [aws_route53_record.example_certificate.fqdn]
+//}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.example.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "これはhttpsです"
+      status_code  = "200"
+    }
+  }
+}
+
+
+resource "aws_lb_listener" "redirect_http_to_https" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "8080"
+  protocol          = "HTTP"
+
+  default_action {
+
+    type = "redirect"
+
+    fixed_response {
+      content_type = "text/plain"
+      //      port         = "443"
+      //      protocol     = "HTTPS"
+      //      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_target_group" "example" {
+  name                 = "example"
+  target_type          = "ip"
+  vpc_id               = aws_vpc.example.id
+  port                 = 80
+  protocol             = "HTTP"
+  deregistration_delay = 300
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = 200
+    port                = "traffic-port"
+    protocol            = "HTTP"
+  }
+
+  depends_on = [aws_lb.example]
+}
+
+resource "aws_lb_listener_rule" "example" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+
+  condition {
+    //    field  = "path-pattern"
+    //    values = ["/*"]
+  }
+}
+
+
+
 
 
 resource "aws_vpc" "example" {
@@ -200,7 +329,8 @@ module "example_sg" {
 }
 
 resource "aws_s3_bucket" "alb_log" {
-  bucket = "study-alb-log-pragmatic-terraform"
+  bucket        = "study-alb-log-pragmatic-terraform"
+  force_destroy = true
 
   lifecycle_rule {
     enabled = true

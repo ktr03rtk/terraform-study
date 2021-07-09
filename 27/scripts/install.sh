1b82dba631e8917f1ef4788537274df58b109141
ktr03 <ktrsouth@gmail.com>
2021-07-10 00:18:26 +0900

    modify path

--- 27/scripts/install.sh
+++ 27/scripts/install.sh
@@ -5,4 +5,4 @@ BASE_URL=https://github.com/mercari/tfnotify/releases/download
 DOWNLOAD_URL="${BASE_URL}/v0.7.0/tfnotify_linux_amd64.tar.gz"
 wget ${DOWNLOAD_URL} -P /tmp
 tar zxvf /tmp/tfnotify_linux_amd64.tar.gz -C /tmp
-mv /tmp/tfnotify_linux_amd64/tfnotify /usr/local/bin/tfnotify
+mv /tmp/tfnotify /usr/local/bin/tfnotify

0521037ad8dccd875b1d3616e5fd5e8e9a7627ea
ktr03 <ktrsouth@gmail.com>
2021-07-10 00:08:04 +0900

    capitalize option

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,6 @@
 BASE_URL=https://github.com/tfnotify/releases/download
 
 DOWNLOAD_URL="${BASE_URL}/v0.3.1/tfnotify_v0.3.1_linux_amd64.tar.gz"
-wget ${DOWNLOAD_URL} -p /tmp
+wget ${DOWNLOAD_URL} -P /tmp
 tar zxvf /tmp/tfnotify_v0.3.1_linux_amd64.tar.gz -C /tmp
 mv /tmp/tfnotify_v0.3.1_linux_amd64/tfnotify /usr/local/bin/tfnotify
